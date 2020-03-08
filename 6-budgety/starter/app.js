f2c0c33288ff7f15f0e62c55136d67c08f69d7b2
ppdraga <ppdraga@mail.ru>
2020-03-08 12:03:55 +0300

    2 iife

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,19 @@
+var budgetComtroller = (function() {
+
+    var x = 23;
+
+    var add = function (a) {
+        return x + a
+    }
+
+    return {
+        publicTest: function(b) {
+            console.log(add(b));
+        }
+    }
+
+})();
+
+var UIController = (function() {
+    // code
+})();
