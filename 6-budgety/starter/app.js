afd5b18dae035c796d26065b6b69b1c02dede14c
ppdraga <ppdraga@mail.ru>
2020-03-09 21:58:10 +0300

    2020-03-09

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,24 +1,90 @@
+
+// BUDGET CONTROLLER
 var budgetCocmtroller = (function() {
 
-    var x = 23;
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
-    var add = function (a) {
-        return x + a
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+    };
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+    // code
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+    return {
+        getinput: function() {
+            return {
+                type : document.querySelector(DOMStrings.inputType).value,
+                description : document.querySelector(DOMStrings.inputType).value,
+                value : document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+        getDOMStrings: function() {
+            return DOMStrings;
+        }
+    };
+})();
+
+// APP MAIN CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMStrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            };
+        });
+    };
+
+    var ctrlAddItem = function() {
+        console.log('add item');
+        // получить данные о доходах-расходах из dom
+        var input = UICtrl.getinput();
+        console.log(input);
+        //add item to budget controller
+
+        //add item to UI
+
+        //calc the budget
+
+        // display the budget on the UI
     }
 
     return {
-        publicTest: function(b) {
-            console.log(add(b));
+        init: function() {
+            console.log('init');
+            setupEventListeners();
         }
     }
-
-})();
-
-var UIController = (function() {
-    // code
-})();
-
-var controller = (function(budgetCtrl, UICtrl) {
-
+    
 
 })(budgetCocmtroller, UIController);
+
+
+controller.init();
